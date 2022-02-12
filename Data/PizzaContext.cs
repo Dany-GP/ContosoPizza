008f47aa6d457e9db196c1752228b5a99acda6f2
Daniel García Paniagua <danielgarcia488.dg@gmail.com>
2022-02-12 17:00:24 -0600

    initial commit

---
+++ Data/PizzaContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Data;
+
+public class PizzaContext : DbContext
+{
+    public PizzaContext (DbContextOptions<PizzaContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(@"Server=localhost;database=contosopizza;uid=root;pwd='';",
+        ServerVersion.AutoDetect(@"Server=localhost;database=contosopizza;uid=root;pwd='';") );
+        Console.WriteLine("\nBase de datos\n");
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+
+    public DbSet<Pizza> Pizzas => Set<Pizza>();
+    public DbSet<Topping> Toppings => Set<Topping>();
+    public DbSet<Sauce> Sauces => Set<Sauce>();
+}
